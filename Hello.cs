da6bff8812df0ea9e10f879d916f9c4db498c5ab
muhammadwaqas3297 <31483728+muhammadwaqas3297@users.noreply.github.com>
2017-08-30 09:46:47 -0700

    Add files via upload

---
+++ Hello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Hello_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ My_Name.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Name is Waqas ");
+            Console.ReadKey();
+        }
+    }
+}
